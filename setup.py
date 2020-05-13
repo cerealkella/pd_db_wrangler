7071f959c382cc90e66487aeb7a678c53753b645
Justin <kellerjustin@protonmail.com>
2020-05-13 17:40:11 -0500

    Bump version: 0.3.0 → 0.4.0

--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/kellerjustin/pd_db_wrangler",
-    version="version='0.3.0'",
+    version="version='0.4.0'",
     zip_safe=False,
 )
