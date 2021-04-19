7c18f0c742e610d1ab08991b0c4681c987d108e3
Justin <kellerjustin@protonmail.com>
2021-04-19 16:54:46 -0500

    Bump version: 0.9.0 → 0.10.0

--- pd_db_wrangler/__init__.py
+++ pd_db_wrangler/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Justin Keller"""
 __email__ = "kellerjustin@protonmail.com"
-__version__ = "__version__ = '0.9.0'"
+__version__ = "__version__ = '0.10.0'"
 
 
 from .pd_db_wrangler import Pandas_DB_Wrangler
