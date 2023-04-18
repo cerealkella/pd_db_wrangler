daba9df0ccbeaff0e673af22479438c6feb39ddb
Justin <kellerjustin@protonmail.com>
2023-04-18 17:16:00 -0500

    WIP

--- pd_db_wrangler/pd_db_wrangler.py
+++ pd_db_wrangler/pd_db_wrangler.py
@@ -3,6 +3,7 @@ import mimetypes
 from pathlib import Path
 
 import pandas as pd
+import tomli
 from sqlalchemy import create_engine
 from sqlalchemy.sql import text
 
@@ -16,6 +17,7 @@ class Pandas_DB_Wrangler:
     """
 
     def __init__(self, connect_string=""):
+        self.options = {}
         if connect_string != "":
             self.connect_string = self.set_connection_string(connect_string)
             self.engine = create_engine(self.connect_string)
@@ -35,18 +37,85 @@ class Pandas_DB_Wrangler:
         else:
             return url
 
+    def pandas_toml_extractor(self, sql: str) -> str:
+        """
+        Looks for comments in a SQL file to help pandas determine types.
+        Example SQL comment:
+        /*pandas*
+        [parse_date]
+        parse_dates = []
+        index_col = ["created_at"]
+        [dtype]
+        user_name = "string"
+        user_id = "int64"
+        created_at = "datetime64[ns, UTC]"
+        [timezone]
+        tz = "America/Chicago"
+        *pandas*/
+
+        Args:
+            sql (str): SQL to parse
+
+        Returns:
+            str: string containing only the TOML to parse
+        """
+        toml_text = sql[
+            sql.find(start := "/*pandas*") + len(start) : sql.find("*pandas*/")
+        ]
+        try:
+            toml_dict = tomli.loads(toml_text)
+        except tomli.TOMLDecodeError:
+            print("No valid toml found in SQL")
+        return toml_dict
+
     def read_sql_file(self, filename):
         """Read SQL from File"""
         path = Path(filename)
-        return path.read_text(encoding="utf-8")
+        sql = path.read_text(encoding="utf-8")
+        self.options = self.pandas_toml_extractor(sql)
+        return sql
 
-    def df_fetch(self, sql, index_col=None, parse_dates=None, dtype=None):
+    def df_fetch(
+        self,
+        sql,
+        index_col=None,
+        parse_dates=None,
+        dtype=None,
+    ):
         """
         Run SQL query on a database with SQL as a parameter
         Please specify connect string and db type using the
         set_connection_string function.
         """
+        options = ("parse_dates", "dtype", "index_col")
+
+        local_args = print(locals().keys())
+        for option in options:
+            try:
+                if parse_dates is None:
+                    parse_dates = self.options["parse_dates"]
+                if dtype is None:
+                    dtype = self.options("dtype")
+                if index_col is None:
+                    index_col = self.options("index_col")
+            except KeyError:
+                pass
+
         with self.engine.begin() as conn:
             return pd.read_sql(
-                sql=text(sql), con=conn, index_col=index_col, parse_dates=parse_dates, dtype=dtype
+                sql=text(sql),
+                con=conn,
+                index_col=index_col,
+                parse_dates=parse_dates,
+                dtype=dtype,
             )
+
+
+pdw = Pandas_DB_Wrangler()
+sql = pdw.read_sql_file("pd_db_wrangler/test_query.sql")
+print(sql)
+print(pdw.options)
+
+parse_dates = pdw.options["parse_date"]
+dtype = pdw.options["dtype"]
+print(dtype)
---
+++ pd_db_wrangler/test_query.sql
@@ -0,0 +1,13 @@
+/*pandas*
+ [parse_date]
+ parse_dates = []
+ index_col = ["created_at"]
+ [dtype]
+ user_name = "string"
+ user_id = "int64"
+ created_at = "datetime64[ns, UTC]"
+ [timezone]
+ tz = "America/Chicago"
+ *pandas*/
+select *
+from history
